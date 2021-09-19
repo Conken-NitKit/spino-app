55499555af065e7c8c60babd088f99656ee7024f
shu20031026 <shu20031026@gmail.com>
2021-09-19 17:06:41 +0900

    コメントの追加

--- src/Login.tsx
+++ src/Login.tsx
@@ -3,7 +3,14 @@ import React from "react";
 const Login = () =>{
     return (
         <div>
-            login
+            <p>
+                ここにFigmaのhome画面を作ってください
+                入力やボタン, ダークモードの機能などは後回しで構いません。
+            </p>
+            <p>
+                urlの部分に /game と入れるとゲームの画面に移ります               
+            </p>  
+            （この文章は不要なので作成時に消してください）          
         </div>
     );
 }
