20ccfdde920382c8e597f3c469eb11511bb57c47
shu20031026 <shu20031026@gmail.com>
2021-09-19 22:09:01 +0900

    タイピングゲームのひな型作成

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,18 +1,20 @@
 import React, {useState} from "react";
 import { dummyText } from "./dummys/dummytext";
 
-//問題をランダムに選択
 const twiiterText : string[] = dummyText;
-const rnd : number = Math.floor(Math.random() * twiiterText.length);
-const question : string = twiiterText[rnd];
 
 const Game = () =>{
-    const [form, setForm] = useState<string>("")
+    const [questionNum, setQuestionNum] = useState<number>(0);
+    const [question,setQuestion] = useState<string>(twiiterText[questionNum]);
+    const [form, setForm] = useState<string>("");
     //正解不正解の判定(consoleに表示)
+
     const result = () =>{
         if(form === question){
             console.log("正解");
-        } else {
+            setQuestionNum(questionNum + 1);
+            setQuestion(twiiterText[questionNum + 1]);
+        } else { 
             console.log("不正解");
         }
     }
@@ -20,12 +22,12 @@ const Game = () =>{
     //html
     return(
         <div>
+            <h1>{questionNum + 1}問目</h1>
             <div>
                 ここにはFigmaのリプ風という項目を作ってください。
-                
             </div>
             <h1>{question}</h1>
-            <input onChange={(e)=> setForm(e.target.value)}></input>
+            <input id="form" onChange={(e)=> setForm(e.target.value)}></input>
             <button onClick={()=> {result()}}>送信</button>
         </div>
     );
