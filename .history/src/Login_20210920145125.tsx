0f3104fe3812962b727f3dbdfca0b4ce6b009c98
hyu-ga <hyu-ga>
2021-09-20 21:51:09 +0900

    ログイン画面のスタイリング

---
+++ .history/src/Login_20210920145125.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const titleStyle = {
+    font-family: PT
+}
+
+const Login = () =>{
+    return (
+        <div>
+            <h1>
+                twippy
+            </h1>
+            <p>
+                twitterのスクリーンネームを入力してください。（例:  @tokotoko5_24）<br />
+                最新のツイート１０件からタイピングゲームが生成されます。
+            </p>
+            <input />
+            <button>
+                ゲームを始める
+            </button>
+        </div>
+    );
+}
+
+export default Login;
---
+++ .history/src/Login_20210920145423.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { reduceEachLeadingCommentRange } from "typescript";
+
+const titleStyle = {
+}
+
+const Login = () =>{
+    return (
+        <div>
+            <h1 style={{ color: backg}}>
+                twippy
+            </h1>
+            <p>
+                twitterのスクリーンネームを入力してください。（例:  @tokotoko5_24）<br />
+                最新のツイート１０件からタイピングゲームが生成されます。
+            </p>
+            <input />
+            <button>
+                ゲームを始める
+            </button>
+        </div>
+    );
+}
+
+export default Login;
