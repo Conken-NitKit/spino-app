5bd73153096dc0d413fb453b03f0c0de188fa686
kototo522 <huyejiben@gmail.com>
2021-09-20 13:14:27 +0900

    add: 影の濃さの調節

--- src/Game.tsx
+++ src/Game.tsx
@@ -14,7 +14,7 @@ const Header = styled.div`
     height: 54px;
     width: 100vw;
     margin-bottom: 50px;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.30);
     @media screen and (max-width: 767px) {
         margin-bottom: 20px;
     }
