35a7175afcbf2d038e43e96405c1479785aadd92
Kajikiryoma <k21041rk@DESKTOP-J990S3R.localdomain>
2021-09-20 23:01:37 +0900

    エンターキーで送信

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,5 +1,6 @@
-import React, {useState} from "react";
+import React, {useState,useRef} from "react";
 import { dummyText } from "./dummys/dummytext";
+
 
 const twiiterText : string[] = dummyText;
 
@@ -7,17 +8,23 @@ const Game = () =>{
     const [questionNum, setQuestionNum] = useState<number>(0);
     const [question,setQuestion] = useState<string>(twiiterText[questionNum]);
     const [form, setForm] = useState<string>("");
+    const startTime = Date.now();
+    let nowTime = 0;
+
+    nowTime = (Date.now() - startTime) / 1000
     //正解不正解の判定(consoleに表示)
 
     const result = () =>{
         if(form === question){
             console.log("正解");
             setQuestionNum(questionNum + 1);
-            setQuestion(twiiterText[questionNum + 1]);
+            setQuestion(twiiterText[questionNum + 1])
+
         } else { 
             console.log("不正解");
         }
     }
+
 
     //html
     return(
@@ -26,8 +33,10 @@ const Game = () =>{
             <div>
                 ここにはFigmaのリプ風という項目を作ってください。
             </div>
-            <h1>{question}</h1>
-            <input id="form" onChange={(e)=> setForm(e.target.value)}></input>
+            <h1>{question}</h1>          
+            <input id="form" onChange={(e)=> setForm(e.target.value)} 
+             onKeyDown={(e) => {if (e.key === 'Enter') {result()}
+            }} />
             <button onClick={()=> {result()}}>送信</button>
         </div>
     );
