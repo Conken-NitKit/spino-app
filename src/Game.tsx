04f9df9f5d2d9b026b8e0fcf6f34d9502e407f11
Kajikiryoma <ryomakaji1014@gmail.com>
2021-09-22 00:46:03 +0900

    時計の機能追加をしました。

--- src/Game.tsx
+++ src/Game.tsx
@@ -1,140 +1,61 @@
-<<<<<<< HEAD
-import React, {useState,useRef} from "react";
+import React, {useState,useRef, useEffect} from "react";
 import { dummyText } from "./dummys/dummytext";
 
 
 const twiiterText : string[] = dummyText;
-=======
-import React, { useState } from "react";
-import styled from "styled-components";
-import { dummyText } from "./dummys/dummytext";
 
-const Container = styled.div`
-    @import url('http://fonts.googleapis.com/earlyaccess/notosansjp.css');
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-`;
->>>>>>> efb4069c6a01df89dcded639550341b557876c64
-
-const Header = styled.div`
-    height: 54px;
-    width: 100vw;
-    margin-bottom: 50px;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.30);
-    @media screen and (max-width: 767px) {
-        margin-bottom: 20px;
-    }
-`;
-
-const QuestionNumText = styled.h1`
-    font-family: 'Noto Sans JP', sans-serif;
-    font-weight: bold;
-    color: #59B4C8;
-    text-align: center;
-`;
-
-const TweetBox = styled.div`
-    display: flex;
-    margin: 20px 0 0 40px;
-    @media screen and (max-width: 767px) {
-        margin: 10px 0 0 5px;
-    }
-`;
-
-const HumanIcon = styled.img`
-    height: 68px;
-    width:  68px;
-    border-radius: 50%;
-`;
-
-const VerticalLine = styled.div`
-    height: 160px;
-    width: 6px;
-    margin: 10px 0 0 31px;
-    background-color: #D9D9D9;
-`;
-
-const Text = styled.p`
-    font-family: 'Noto Sans JP', sans-serif;
-    font-weight: 400;
-    margin-left: 20px;
-    font-size: 30px;
-    @media screen and (max-width: 767px) {
-    font-size: 25px;
-    }
-`;
-
-const TextArea = styled.textarea`
-    height: 200px;
-    width: calc(85vw - 64px);
-    background-color: #F9F9F9;
-    font-size: 24px;
-    font-family: 'Noto Sans JP', sans-serif;
-    margin-left: 20px;
-    resize: none;
-    border: none;
-    outline-color: #D9D9D9;
-`;
-
-const Sending = styled.button`
-    height: 40px;
-    width: 120px;
-    margin: 40px 100px 0 0;
-    float: right;
-    background-color: #59B4C8;
-    font-size: 20px;
-    font-family: 'Noto Sans JP', sans-serif;
-    font-weight: 700;
-    color: #FFFFFF;
-    border: none;
-    &:hover {
-        border: 2px solid rgba(89, 180, 200, 0.7);
-        box-sizing: border-box;
-        box-shadow: 0px 10px 10px rgba(89, 180, 200, 0.25);
-        background-color: #98D2DF;
-  }
-  @media screen and (max-width: 767px) {
-      margin: 20px auto;
-    }
-`;
-
-const twiiterText: string[] = dummyText;
-
-const Game = () => {
+const Game = () =>{
     const [questionNum, setQuestionNum] = useState<number>(0);
-    const [question, setQuestion] = useState<string>(twiiterText[questionNum]);
+    const [question,setQuestion] = useState<string>(twiiterText[questionNum]);
     const [form, setForm] = useState<string>("");
-<<<<<<< HEAD
-    const startTime = Date.now();
-    let nowTime = 0;
-
-nowTime = (Date.now() - startTime) / 1000
-=======
-    const [missCount, setMissCount] = useState<number>(0);
->>>>>>> efb4069c6a01df89dcded639550341b557876c64
     //正解不正解の判定(consoleに表示)
 
-    const result = () => {
-        if (form === question) {
+    const result = () =>{
+        if(form === question){
             console.log("正解");
             setQuestionNum(questionNum + 1);
-<<<<<<< HEAD
             setQuestion(twiiterText[questionNum + 1])
 
-=======
-            setQuestion(twiiterText[questionNum + 1]);
-            setForm("");
->>>>>>> efb4069c6a01df89dcded639550341b557876c64
         } else { 
             console.log("不正解");
-            setMissCount(missCount + 1);          
         }
     }
-<<<<<<< HEAD
-
-
+    
+    const watch = () =>{
+        var timer = document.getElementById('timer');
+        var start = document.getElementById('start');
+        var stop = document.getElementById('stop');
+        var reset = document.getElementById('reset');
+        const startTime = new Date();
+        var timerId;
+        var elapsedTime = 0;
+        var timeToadd = 0;
+        function updateTimetText(){
+            var minus = Math.floor(elapsedTime / 60000);
+            var second = Math.floor(elapsedTime % 60000 / 1000);
+            var msecond = elapsedTime % 1000;
+            timer!.textContent = minus + ':' + second + ':' + msecond;
+        }
+        function countUp(){
+            timerId = setTimeout(function(){
+            elapsedTime = Date.now()  + timeToadd;
+            updateTimetText()
+            countUp();
+            },10);
+            }
+            start!.addEventListener('click',function(){
+            countUp();
+            });
+            stop!.addEventListener('click',function(){
+            timeToadd += Date.now() - timeToadd;
+            });
+            reset!.addEventListener('click',function(){
+            elapsedTime = 0;
+            timeToadd = 0;
+            updateTimetText();
+            });
+    }
+ 
     //html
     return(
         <div>
@@ -147,36 +68,14 @@ nowTime = (Date.now() - startTime) / 1000
              onKeyDown={(e) => {if (e.key === 'Enter') {result()}
             }} />
             <button onClick={()=> {result()}}>送信</button>
+            <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" className="twitter-share-button" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
+            <div id="timer">00:00:000</div>
+            <button id="start">start</button>
+            <button id="stop">stop</button>
+            <button id="reset">reset</button>
+            <script src="js/main.js"></script>
+
         </div>
-=======
-            
-    return (
-        <Container>
-            <Header>
-                <QuestionNumText>{questionNum + 1}問目</QuestionNumText>
-            </Header>
-            <div>{missCount}問不正解</div>
-            <TweetBox>
-                <div>
-                    <HumanIcon src="https://pendelion.com/wp-content/uploads/2021/04/712e65b68b3db426ad0e5aebfddecfcb.png" />
-                    <VerticalLine />
-                </div>
-                <div>
-                    <Text style={{ color: "#5B5B5B" }}>あなたのツイート</Text>
-                    <Text style={{ fontSize: "24px" }}>{question}</Text>
-                </div>
-            </TweetBox >
-            <TweetBox>
-                <div>
-                    <HumanIcon src="https://pendelion.com/wp-content/uploads/2021/04/712e65b68b3db426ad0e5aebfddecfcb.png" />
-                </div>
-                <div>
-                    <TextArea placeholder={"入力してください"} id="form" value={form} onChange={(e) => setForm(e.target.value)}></TextArea>
-                </div>
-            </TweetBox>
-            <Sending onClick={() => { result() }}>送信</Sending>
-        </Container>
->>>>>>> efb4069c6a01df89dcded639550341b557876c64
     );
 }
 
